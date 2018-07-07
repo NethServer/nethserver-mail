30d97e0b8346d7c511f75f4b54878e9237d9c800
Davide Principi <davide.principi@nethesis.it>
2018-07-07 09:34:47 +0200

    Use Accordion to display DKIM procedures (#5)
    NethServer/dev#5539

--- common/usr/share/nethesis/NethServer/Template/Mail/Domain/Modify.php
+++ common/usr/share/nethesis/NethServer/Template/Mail/Domain/Modify.php
@@ -7,10 +7,12 @@ $view->requireFlag($view::INSET_FORM);
 if ($view->getModule()->getIdentifier() == 'update') {
     $headerText = $T('Update domain `${0}`');
     $messagesText = $T('Messages to domain ${0}');
+    $dkimDomainSetupText = $T('DkimEditDomainSetup_label');
     $keyStyles = $view::STATE_READONLY;
 } else {
     $headerText = $T('Create a new domain');
     $messagesText = $T('Messages to this domain');
+    $dkimDomainSetupText = $T('DkimNewDomainSetup_label');
     $keyStyles = 0;
 }
 
@@ -49,21 +51,57 @@ if (@file_exists('/usr/libexec/nethserver/disclaimer-send')) {
         ->insert($view->textArea('DisclaimerText', $view::LABEL_NONE)->setAttribute('dimensions', '10x40'));
 }
 
+$accordionId = $view->getUniqueId('Accordion');
 if(@file_exists('/etc/e-smith/db/configuration/defaults/dovecot/type')) {
+    $panelTemplate = '<h3><a href="#">%s</a></h3><div>%s</div>';
+    $dkimFlat = sprintf($panelTemplate,
+        $view->textLabel('DkimSelector')->setAttribute('template', $T('TxtRecordFlat_label')),
+        $view->textLabel('DkimKey')
+    );
+    $dkimRaw = sprintf($panelTemplate,
+        $view->textLabel('DkimSelector')->setAttribute('template', $T('TxtRecordRaw_label')),
+        $view->textLabel('DkimKeyRaw')
+    );
+
     echo $view->fieldsetSwitch('OpenDkimStatus', 'enabled', $view::FIELDSETSWITCH_EXPANDABLE | $view::FIELDSETSWITCH_CHECKBOX)
         ->setAttribute('uncheckedValue', 'disabled')
-        ->insert($view->textLabel('DkimSelector')->setAttribute('tag', 'div')->setAttribute('template', $T('DkimSelector_label')))
-        ->insert($view->textArea('DkimKey', $view::LABEL_ABOVE|$view::STATE_READONLY)->setAttribute('dimensions', '10x80'))
-        ->insert($view->fieldset(NULL, $view::FIELDSET_EXPANDABLE)->setAttribute('template', $T('Advanced_label'))
-        ->insert($view->textArea('DkimKeyRaw', $view::LABEL_ABOVE|$view::STATE_READONLY)->setAttribute('dimensions', '10x80')));
+        ->insert($view->textLabel('domain')->setAttribute('tag', 'div')->setAttribute('template', $dkimDomainSetupText))
+        ->insert($view->panel()->setAttribute('id', $accordionId)
+            ->insert($view->literal($dkimFlat))
+            ->insert($view->literal($dkimRaw))
+        )
+    ;
 }
 
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP | $view::BUTTON_CANCEL);
+
+$openDkimStatusId = $view->getUniqueId('OpenDkimStatus');
 
 $view->includeJavascript("
 (function ( \$ ) {
     \$('.${domainTarget}').on('nethguiupdateview', function(ev, domain) {
         \$('.${transportTypeTarget}[value=Relay]').trigger(domain == $jsPrimaryDomain ? 'nethguidisable' : 'nethguienable');
     });
+    \$('#${accordionId}').accordion();
+    \$(window.document).on('nethguishow', function(ev, value) {
+        \$('#${accordionId}').accordion('resize');
+    });
+    \$('#${openDkimStatusId}').parent().next().on('nethguienable', function(ev, value) {
+        window.setTimeout(function(){\$('#${accordionId}').accordion('resize')}, 0);
+    });
 } ( jQuery ));
+");
+
+$view->includeCss("
+#${accordionId} {
+    margin-top: 0.5em;
+    max-width: 80em;
+}
+#${accordionId} .ui-state-active {
+    border-color: #aaa;
+}
+#${accordionId} .ui-accordion-content {
+    word-break: break-all;
+    font-family: monospace;
+}
 ");
