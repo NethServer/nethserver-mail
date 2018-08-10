395640edf1ce9a52783fef6ee97f85cab79c2b2e
Stephane de Labrusse <stephdl@de-labrusse.fr>
2018-08-10 18:17:14 +0200

    Subject Lenght UI validator and status watch (#63)
    NethServer/dev#5562

--- filter/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Mail_Filter.php
+++ filter/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Mail_Filter.php
@@ -31,3 +31,4 @@ $L['rspamdWebUI_red_label'] = 'Access from external network';
 $L['RspamdURL'] = 'Web URL';
 $L['RspamdPassword_label'] = 'Password';
 $L['Rspamd_WebUI_Settings_label'] = 'Rspamd user interface';
+$L['SpamSubjectPrefixString_label'] = 'Add a prefix to spam messages subject'; 
