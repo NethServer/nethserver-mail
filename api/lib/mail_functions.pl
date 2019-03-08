88d717d7985c1be493da8444d84bb21df400bd02
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2019-03-08 11:46:12 +0100

    api: add dashboard read

---
+++ api/lib/mail_functions.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+#
+# Copyright (C) 2019 Nethesis S.r.l.
+# http://www.nethesis.it - nethserver@nethesis.it
+#
+# This script is part of NethServer.
+#
+# NethServer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or any later version.
+#
+# NethServer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NethServer.  If not, see COPYING.
+#
+
+use strict;
+use warnings;
+use JSON;
+
+sub read_rspamd
+{
+    my $api = shift;
+    return decode_json(`curl 'http://localhost:11334/$api' 2>/dev/null`);
+}
+
+
+1;
