3c7624a11385577e89e880c01a3299039197f868
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2019-03-14 15:58:36 +0100

    UI: menu scaffold

--- ui/src/main.js
+++ ui/src/main.js
@@ -24,6 +24,11 @@ import Router from 'vue-router'
 
 import App from './App.vue'
 import Dashboard from './views/Dashboard.vue'
+import Queue from './views/Queue.vue'
+import Filter from './views/Filter.vue'
+import Transport from './views/Transport.vue'
+import Addresses from './views/Addresses.vue'
+import Mailboxes from './views/Mailboxes.vue'
 import Logs from './views/Logs.vue'
 import About from './views/About.vue'
 
@@ -44,10 +49,15 @@ const router = new Router({
     mode: 'hash',
     base: process.env.BASE_URL,
     routes: [
-        { path: '/', redirect: '/dashboard'},
-        { path: '/dashboard', component: Dashboard },
-        { path: '/logs', component: Logs },
-        { path: '/about', name: 'about', component: About },
+      { path: '/', redirect: '/dashboard'},
+      { path: '/dashboard', component: Dashboard },
+      { path: '/queue', component: Queue },
+      { path: '/filter', component: Filter },
+      { path: '/transport', component: Transport },
+      { path: '/mailboxes', component: Mailboxes },
+      { path: '/addresses', component: Addresses },
+      { path: '/logs', component: Logs },
+      { path: '/about', name: 'about', component: About },
     ]
 })
 router.replace("/dashboard")
