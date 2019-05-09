38b45a204048c48f1da21fc63ed7e73af7b7f97b
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2019-05-09 09:36:23 +0200

    ui. preserve route on page reload

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -141,6 +141,15 @@
 <script>
 export default {
   name: "App",
+  watch: {
+    $route: function(val) {
+      localStorage.setItem("path", val.path);
+    }
+  },
+  mounted() {
+    var path = localStorage.getItem("path") || "/";
+    this.$router.push(path);
+  },
   methods: {
     getCurrentPath(route, offset) {
       if (offset) {
