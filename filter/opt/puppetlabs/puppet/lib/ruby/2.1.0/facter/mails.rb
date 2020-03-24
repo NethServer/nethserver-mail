5f12f905344287ac13750984cbf83aeabff5d1d4
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2020-03-24 11:43:04 +0100

    facter: add mail counters

---
+++ filter/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/mails.rb
@@ -0,0 +1,34 @@
+#
+# mails
+# Count the number of mails
+#
+
+require 'date'
+
+Facter.add('mails') do
+    confine osfamily: 'RedHat'
+    setcode do
+        file = File.foreach("/var/log/maillog")
+        yesterday = Time.now - (24*60*60)
+        mails = { "total" => 0, "attachments" => 0, "received" => 0, "sent" => 0}
+        file.each_entry do |line|
+            parts = line.split()
+            date = DateTime.parse(parts[0]+" "+parts[1]+" "+parts[2])
+            if date.to_time >= yesterday
+                if line.include? "HAS_ATTACHMENT"
+                    mails['attachments'] = mails['attachments'] + 1;
+                end
+
+                if line.include? "status=sent"
+                    mails['total'] = mails['total']+1
+                    if line.match('postfix\/lmtp\[.*status=sent')
+                        mails['received'] = mails['received']+1
+                    elsif line.match('postfix\/smtp\[.*status=sent')
+                        mails['sent'] = mails['sent']+1
+                    end
+                end
+            end
+        end
+        mails
+    end
+end
